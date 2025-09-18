65373a6d37f897ff53e176d770f3c74b63c74f28
Joel Smith <joelsmith2004@gmail.com>
2025-09-19 06:47:32 +0800

    feat(added more routes)

--- Commands/General.cs
+++ Commands/General.cs
@@ -5,6 +5,24 @@ public class GeneralCommands
 {
     public void AddGeneralCommands()
     {
+        Settings();
+    }
 
+    public void Settings()
+    {
+        var cmd = new Command("app-settings", "Gets Chronos App Settings");
+        cmd.SetHandler(async () =>
+        {
+            var res = await ApiService.Instance.GetRoute("/app-settings/basic");
+            if (res.Success)
+            {
+                AnsiConsole.MarkupLine($"[green]Response:[/] {Markup.Escape(res.Content)}");
+            }
+            else
+            {
+                AnsiConsole.MarkupLine($"[red]Error {res.StatusCode}[/]");
+            }
+        });
+        RootCommandService.Instance.AddCommand(cmd);
     }
 }
--- Commands/User.cs
+++ Commands/User.cs
@@ -6,7 +6,8 @@ public class UserCommands
     public void AddUserCommands()
     {
         GetMe();
-
+        CurTimeEntries();
+        GetProjects();
     }
 
     public void GetMe()
@@ -17,7 +18,7 @@ public class UserCommands
             var res = await ApiService.Instance.GetRoute("/users/me");
             if (res.Success)
             {
-                AnsiConsole.MarkupLine($"[green]Response:[/] {res.Content}");
+                AnsiConsole.MarkupLine($"[green]Response:[/] {Markup.Escape(res.Content)}");
             }
             else
             {
@@ -26,4 +27,43 @@ public class UserCommands
         });
         RootCommandService.Instance.AddCommand(getUserCmd);
     }
+
+    public void CurTimeEntries()
+    {
+        var cmd = new Command("entries", "Returns Current Time Entries");
+        cmd.SetHandler(async () =>
+        {
+            // /time-entry/<userid>?minDate=<monday>&maxDate=<sunday> generally like this 
+            // can just change the dates to whatever size you want
+            var res = await ApiService.Instance.GetRoute("/time-entry/166?minDate=2025-09-15&maxDate=2025-09-21"); // TODO remove hard coded ID maybe store in config
+            if (res.Success)
+            {
+                AnsiConsole.MarkupLine($"[green]Response:[/] {Markup.Escape(res.Content)}");
+            }
+            else
+            {
+                AnsiConsole.MarkupLine($"[red]Error {res.StatusCode}[/]");
+            }
+        });
+        RootCommandService.Instance.AddCommand(cmd);
+    }
+
+    public void GetProjects()
+    {
+        var cmd = new Command("projects", "Returns Assigned Projects");
+        cmd.SetHandler(async () =>
+        {
+            var res = await ApiService.Instance.GetRoute("/task-user/166"); // TODO remove hard coded ID maybe store in config
+            if (res.Success)
+            {
+                AnsiConsole.MarkupLine($"[green]Response:[/] {Markup.Escape(res.Content)}");
+            }
+            else
+            {
+                AnsiConsole.MarkupLine($"[red]Error {res.StatusCode}[/]");
+            }
+        });
+        RootCommandService.Instance.AddCommand(cmd);
+    }
+
 }
